a2ff972f60b0c5bba020a543e4a8f99d28273848
gyf <charon0707@163.com>
2023-03-26 00:38:13 +0800

    mytop finished

--- Lab1_Shell/myshell.c
+++ Lab1_Shell/myshell.c
@@ -3,20 +3,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
+#include <sys/wait.h>   /*under Linux*/
 #include <sys/types.h>  /*system calls*/
-#include <string.h>   /*strtok*/
+#include <string.h>    
+#include <dirent.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
 
+/*under Minix*/
+#include <minix/com.h>
+#include <minix/config.h>
+#include <minix/type.h>
+#include <minix/endpoint.h>
+#include <minix/const.h>
+#include <minix/u64.h>
+#include <paths.h>
+#include <minix/procfs.h>
+
 #define true 1   
 #define MAXLINE 1024 /* max size of cmdline */
 #define MAXPARA 32  /*max number of parameters*/
-#define HISTORY_N  10 /*max number of history cmd*/ /*dynamic malloc*/
+#define HISTORY_N  100 /*max number of history cmd*/ /*dynamic malloc*/
 #define MEMOINFO_LEN 5
-#define INVALID_CMD 0
 #define BUILDIN_CMD 1
 #define REDIRECT_in_CMD 2
 #define REDIRECT_out_CMD 3
@@ -24,12 +34,47 @@
 #define PROGRAM_CMD 5
 #define STD_INPUT 0 /* file descriptor for standard input */
 #define STD_OUTPUT 1 /* file descriptor for standard output */
+const char *cputimenames[] = { "user", "ipc", "kernelcall" };
+#define CPUTIMENAMES (sizeof(cputimenames)/sizeof(cputimenames[0]))
+#define PROC_NAME_LEN   16
+#define PSINFO_VERSION	0
+#define  SLOT_NR(e) (_ENDPOINT_P(e) + nr_tasks)
+#define MAX_NR_TASKS	1023
+#define _ENDPOINT_GENERATION_SIZE (MAX_NR_TASKS+_MAX_MAGIC_PROC+1)
+#define _ENDPOINT_P(e) \
+	((((e)+MAX_NR_TASKS) % _ENDPOINT_GENERATION_SIZE) - MAX_NR_TASKS)
+#define SELF	((endpoint_t) 0x8ace) 	/* used to indicate 'own process' */
+#define _MAX_MAGIC_PROC (SELF)
+#define PATH_MAX	1024
+/* Process types. */
+#define TYPE_TASK	'T'
+#define TYPE_SYSTEM	'S'
+#define TYPE_USER	'U'
+#define _PATH_PROC	"/proc/"
+#define ORDER_CPU	0
+#define ORDER_MEMORY	1
+#define CPUTIME(m, i) (m & (1L << (i)))
+#define  SLOT_NR(e) (_ENDPOINT_P(e) + nr_tasks)
+#define  USED		0x1
+#define  IS_TASK	0x2
+#define  IS_SYSTEM	0x4
+#define  BLOCKED	0x8
+
+typedef int endpoint_t;			/* process identifier */
+typedef	unsigned long long int u64_t;
+typedef long unsigned int vir_bytes;	/* virtual addresses/lengths in bytes */
 
 /*global variables*/
+int nr_total;
+int order = ORDER_CPU;
+unsigned int nr_procs, nr_tasks;
+
 char prompt[] = "(myshell)# ";
 char** history_cmd;  /*keep all cmdlines*/
 int h = 0; /*number of history cmds*/
 int all_his_num = 0;
+unsigned int nr_procs, nr_tasks;
+
 void print_prompt();
 void read_command(char* commandline);
 int parse(char commandline[], char** cmd, char** parameters, int* argc);
@@ -45,6 +90,27 @@ void callRedirect_in(char* commandline);
 void parse_program(char* input, char** cmd, char** parameter_list);
 void pipeline(char* commamdline);
 void program(char* commandline);
+
+struct tp
+{
+    struct proc *p;
+    u64_t ticks;
+};
+struct proc {
+	int p_flags;
+	endpoint_t p_endpoint;
+	pid_t p_pid;
+	u64_t p_cpucycles[CPUTIMENAMES];
+	int p_priority;
+	endpoint_t p_blocked;
+	time_t p_user_time;
+	vir_bytes p_memory;
+	uid_t p_effuid;
+	int p_nice;
+	char p_name[PROC_NAME_LEN+1];
+};
+
+struct proc *proc = NULL, *prev_proc = NULL;
 
 int main(){
     char* commandline;
@@ -196,7 +262,6 @@ void exec_cd(char** parameters, int argc){
     }
 }
 
-
 void print_history(int argc, char* parameters[MAXPARA]){
     if(argc == 1){
         for(int i =0; history_cmd[i]!=NULL; i++){
@@ -210,6 +275,125 @@ void print_history(int argc, char* parameters[MAXPARA]){
         }        
     }
 }
+
+void parse_file(pid_t pid)
+{
+	char path[PATH_MAX], name[256], type, state;
+	int version, endpt, effuid;
+	unsigned long cycles_hi, cycles_lo;
+	FILE *fp;
+	struct proc *p;
+	int slot;
+	int i;
+
+	sprintf(path, "%d/psinfo", pid);
+
+	if ((fp = fopen(path, "r")) == NULL)
+		return;
+
+	if (fscanf(fp, "%d", &version) != 1) {
+		fclose(fp);
+		return;
+	}
+
+	if (version != PSINFO_VERSION) {
+		fputs("procfs version mismatch!\n", stderr);
+		exit(1);
+	}
+
+	if (fscanf(fp, " %c %d", &type, &endpt) != 2) {
+		fclose(fp);
+		return;
+	}
+
+	slot = SLOT_NR(endpt);
+
+	if(slot < 0 || slot >= nr_total) {
+		// fprintf(stderr, "top: unreasonable endpoint number %d\n", endpt);
+		fclose(fp);
+		return;
+	}
+
+	p = &proc[slot];
+
+	if (type == TYPE_TASK)
+		p->p_flags |= IS_TASK;
+	else if (type == TYPE_SYSTEM)
+		p->p_flags |= IS_SYSTEM;
+
+	p->p_endpoint = endpt;
+	p->p_pid = pid;
+
+	if (fscanf(fp, " %255s %c %d %d %lu %*u %lu %lu",
+		name, &state, &p->p_blocked, &p->p_priority,
+		&p->p_user_time, &cycles_hi, &cycles_lo) != 7) {
+
+		fclose(fp);
+		return;
+	}
+
+	strncpy(p->p_name, name, sizeof(p->p_name)-1);
+	p->p_name[sizeof(p->p_name)-1] = 0;
+
+	if (state != STATE_RUN)
+		p->p_flags |= BLOCKED;
+	p->p_cpucycles[0] = make64(cycles_lo, cycles_hi);
+	p->p_memory = 0L;
+
+	if (!(p->p_flags & IS_TASK)) {
+		int j;
+		if ((j=fscanf(fp, " %lu %*u %*u %*c %*d %*u %u %*u %d %*c %*d %*u",
+			&p->p_memory, &effuid, &p->p_nice)) != 3) {
+
+			fclose(fp);
+			return;
+		}
+
+		p->p_effuid = effuid;
+	} else p->p_effuid = 0;
+
+	for(i = 1; i < CPUTIMENAMES; i++) {
+		if(fscanf(fp, " %lu %lu",
+			&cycles_hi, &cycles_lo) == 2) {
+			p->p_cpucycles[i] = make64(cycles_lo, cycles_hi);
+		} else	{
+			p->p_cpucycles[i] = 0;
+		}
+	}
+
+	if ((p->p_flags & IS_TASK)) {
+		if(fscanf(fp, " %lu", &p->p_memory) != 1) {
+			p->p_memory = 0;
+		}
+	}
+
+	p->p_flags |= USED;
+
+	fclose(fp);
+}
+
+void parse_dir(void)
+{
+	DIR *p_dir;
+	struct dirent *p_ent;
+	pid_t pid;
+	char *end;
+
+	if ((p_dir = opendir(".")) == NULL) {
+		perror("opendir on" _PATH_PROC);
+		exit(1);
+	}
+
+	for (p_ent = readdir(p_dir); p_ent != NULL; p_ent = readdir(p_dir)) {
+		pid = strtol(p_ent->d_name, &end, 10);
+
+		if (!end[0] && pid != 0)
+			parse_file(pid);
+	}
+
+	closedir(p_dir);
+}
+
 int print_memory(void)
 {
 	FILE *fp;
@@ -234,9 +418,161 @@ int print_memory(void)
 
 	return 1;
 }
+void get_procs(void)
+{
+	struct proc *p;
+	int i;
 
+	p = prev_proc;
+	prev_proc = proc;
+	proc = p;
+
+	if (proc == NULL) {
+		proc = malloc(nr_total * sizeof(proc[0]));
+
+		if (proc == NULL) {
+			fprintf(stderr, "Out of memory!\n");
+			exit(1);
+		}
+	}
+
+	for (i = 0; i < nr_total; i++)
+		proc[i].p_flags = 0;
+
+	parse_dir();
+}
+u64_t cputicks(struct proc *p1, struct proc *p2, int timemode)
+{
+	int i;
+	u64_t t = 0;
+	for(i = 0; i < CPUTIMENAMES; i++) {
+		if(!CPUTIME(timemode, i))
+			continue;
+		if(p1->p_endpoint == p2->p_endpoint) {
+			t = t + p2->p_cpucycles[i] - p1->p_cpucycles[i];
+		} else {
+			t = t + p2->p_cpucycles[i];
+		}
+	}
+
+	return t;
+}
+
+char *ordername(int orderno)
+{
+	switch(orderno) {
+		case ORDER_CPU: return "cpu";
+		case ORDER_MEMORY: return "memory";
+	}
+	return "invalid order";
+}
+
+void print_procs(struct proc *proc1, struct proc *proc2, int cputimemode)
+{
+    int p, nprocs;
+    u64_t systemticks = 0;
+    u64_t userticks = 0;
+    u64_t total_ticks = 0;
+    u64_t idleticks = 0;
+    u64_t kernelticks = 0;
+    int blockedseen = 0;
+    //创建了一个struct tp的结构体数组
+    static struct tp *tick_procs = NULL;
+    if (tick_procs == NULL)
+    {
+        tick_procs = malloc(nr_total * sizeof(tick_procs[0]));
+        if (tick_procs == NULL)
+        {
+            fprintf(stderr, "Out of memory!\n");
+            exit(1);
+        }
+    }
+    for (p = nprocs = 0; p < nr_total; p++)
+    {
+        u64_t uticks;
+        if (!(proc2[p].p_flags & USED))
+        { //查看USED位是否被标记
+            continue;
+        }
+        tick_procs[nprocs].p = proc2 + p;//初始化
+        //tickprocs的第np个结构体的struct proc *p
+        //为proc2第p个文件的struct proc
+        tick_procs[nprocs].ticks = cputicks(&proc1[p], &proc2[p], cputimemode);
+        uticks = cputicks(&proc1[p], &proc2[p], 1);
+        total_ticks = total_ticks + uticks;
+        if (p - NR_TASKS == IDLE)
+        {
+            idleticks = uticks;
+            continue;
+        }
+        if (p - NR_TASKS == KERNEL)
+        {
+            kernelticks = uticks;
+        }
+        if (!(proc2[p].p_flags & IS_TASK))
+        {
+            //如果是进程，则看是system还是user
+            if (proc2[p].p_flags & IS_SYSTEM)
+            {
+                systemticks = systemticks + tick_procs[nprocs].ticks;
+            }
+            else
+            {
+                userticks = userticks + tick_procs[nprocs].ticks;
+            }
+        }
+        nprocs++;
+    }
+    if (total_ticks == 0)
+    {
+        return;
+    }
+    printf("CPU states: %6.2f%% user, ", 100.0 * userticks / total_ticks);
+    printf("%6.2f%% system, ", 100.0 * systemticks / total_ticks);
+    printf("%6.2f%% kernel, ", 100.0 * kernelticks / total_ticks);
+    printf("%6.2f%% idle\n", 100.0 * idleticks / total_ticks);
+}
+
+
+
+
+void getkinfo(void)
+{
+	FILE *fp;
+
+	if ((fp = fopen("kinfo", "r")) == NULL) {
+		fprintf(stderr, "opening " _PATH_PROC "kinfo failed\n");
+		exit(1);
+	}
+
+	if (fscanf(fp, "%u %u", &nr_procs, &nr_tasks) != 2) {
+		fprintf(stderr, "reading from " _PATH_PROC "kinfo failed\n");
+		exit(1);
+	}
+
+	fclose(fp);
+
+	nr_total = (int) (nr_procs + nr_tasks);
+}
+
+void sigwinch(int sig) { }
 void exec_mytop(){
-    print_memory();
+    if (chdir(_PATH_PROC) != 0) {
+		    perror("chdir to " _PATH_PROC);
+		    return ;
+    }
+    int cputimemode = 1;
+    getkinfo();
+    signal(SIGWINCH, sigwinch);
+    while(true){
+        print_memory();
+        get_procs();
+        if(prev_proc == NULL){
+            get_procs();
+        }  
+        print_procs(prev_proc, proc, cputimemode);
+        sleep(2);
+    }
 }
 
 void parse_memoinfo(int* content){
@@ -262,21 +598,22 @@ void parse_memoinfo(int* content){
     }    
 }
 
-void print_meminfo(){
-    int content[MEMOINFO_LEN];
-    parse_memoinfo(content);  /*获取了meminfo 的内容*/
-    int pagesize = content[0];
-    int total = content[1];
-    int free = content[2];
-    int largest = content[3];
-    int cached = content[4];
-    int t_m = (pagesize*total)/1024;
-    int f_m = (pagesize*free)/1024;
-    int l_m = (pagesize*largest)/1024;
-    int c_m = (pagesize*cached)/1024;
-    printf("Main memory: ");
-    printf("%dk total, %dk free, %dk contig free, %dk cached", t_m, f_m, l_m, c_m);
-}
+/*自己实现打印内存信息*/
+// void print_meminfo(){
+//     int content[MEMOINFO_LEN];
+//     parse_memoinfo(content);  /*获取了meminfo 的内容*/
+//     int pagesize = content[0];
+//     int total = content[1];
+//     int free = content[2];
+//     int largest = content[3];
+//     int cached = content[4];
+//     int t_m = (pagesize*total)/1024;
+//     int f_m = (pagesize*free)/1024;
+//     int l_m = (pagesize*largest)/1024;
+//     int c_m = (pagesize*cached)/1024;
+//     printf("Main memory: ");
+//     printf("%dk total, %dk free, %dk contig free, %dk cached", t_m, f_m, l_m, c_m);
+// }
 
 void callRedirect_out(char* commandline){
     // 先确定input 和 output
@@ -473,7 +810,7 @@ void pipeline(char* commamdline){
             //char* pa[]= {"grep", "txt", NULL};
             execvp(cmd_out, para_list_out);
             exit(0);
-        }    	
+        }   	
     }else{
     	wait(NULL);
     }
@@ -490,6 +827,5 @@ void program(char* commandline){
     }else{
         wait(NULL);
     }
-
 }
 
